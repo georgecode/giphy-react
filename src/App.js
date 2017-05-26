c7d9c13d91635ad0f9789f86a846ee773a708837
georgecode <georgeczurlanis@gmail.com>
2017-05-25 19:16:29 -0700

    Add one fetch gif example

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h2>Welcome to React</h2>
-        <p>hello hello hello</p>
         <BodyContainer/>
       </div>
     );
--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,15 +1,63 @@
 import React, {Component} from 'react';
 
 class BodyContainer extends Component{
+	constructor(props,context){
+		super(props,context);
+
+		this.state={
+			title:'HELLO HELLO HELLO',
+			image:'https://tiltify.com/assets/default-avatar-5d89b08b793f205b5fe1d94ae58b00c6449b8b234666f2a29545d5a5cb3d981a.jpg'
+		}
+
+
+		this.displayContent = this.displayContent.bind(this);
+	}
+
+	displayContent(response){
+
+		console.log(response.data[0].images)
+		this.setState({image: response.data[0].images.downsized_medium.url})
+	}
+
+
+	fetchFunction(){
+		// fetch('https://jsonplaceholder.typicode.com/posts/1')
+		// fetch('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC')
+		fetch('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC&limit=10')
+		.then(function(response) {
+			return response.json();
+		}).then(
+
+			this.displayContent
+		).catch(function(error) {
+			console.log("Shits all fucked up in fetchFunction ")
+			// shit went wrong
+		})
+
+	}
+
+
+	componentDidMount(){
+		
+		this.fetchFunction()
+	}
+
+
+
+
+
 	render(){
 		return(
 			<div>
 			
 			<h1>REACT BodyContainer </h1>
+
+			<h1>{this.state.title}</h1>
+			<img src={this.state.image}/>
 			</div>
 			)
 	}
-}
+}//END BodyContainer
 
 
 
