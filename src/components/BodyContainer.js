05ae14b158356f09e61fad870c8d456dd35e52d8
georgecode <georgeczurlanis@gmail.com>
2017-05-25 17:17:16 -0700

    Add Css Reset

---
+++ src/components/BodyContainer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class BodyContainer extends Component{
+	render(){
+		return(
+			<div>
+			<h1>REACT BodyContainer </h1>
+			</div>
+			)
+	}
+}
+
+
+
+export default BodyContainer;
